1ca325cbd6e00b39b553111b1f3d654d741d873f
inazir <imran.nazir@yahoo.co.uk>
2018-11-06 13:25:11 +0000

    initial commit

---
+++ asteroids/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <AsteroidGrid @remove="remove" :asteroids="asteroids" header="Near Earth Objects"/>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import AsteroidGrid from "./components/AsteroidGrid.vue";
+export default {
+  name: "app",
+  components: {
+    AsteroidGrid
+  },
+  data() {
+    return {
+      asteroids: []
+    };
+  },
+  created() {
+    this.fetchAsteroids();
+  },
+  methods: {
+    fetchAsteroids() {
+      var apiKey = "lkgI9to0hRizfzk4xTAxtNTTFkkA4Mtq7y1yW5me";
+      var url = "https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=" + apiKey;
+      axios.get(url).then(res => {
+        this.asteroids = res.data.near_earth_objects.slice(0, 10);
+      });
+    },
+    remove(index) {
+      this.asteroids.splice(index, 1);
+    }
+  }
+};
+</script>
+
+<style>
+[v-cloak] {
+  display: none;
+}
+</style>
