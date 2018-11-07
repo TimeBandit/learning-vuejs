abb78db5406aa9d40ede143fa011194cb7c5ad13
inazir <imran.nazir@yahoo.co.uk>
2018-11-07 15:04:19 +0000

    created my-asteroids

---
+++ my-asteroids/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Hero heading="working"/>
+  </div>
+</template>
+
+<script>
+import Hero from './components/Hero.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Hero,
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ my-asteroids/src/components/Hero.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="header">
+        <div class="header__logo"></div>
+        <div class="header__text">
+            <h1>{{heading}}</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Hero',
+  props: ['heading'],
+};
+</script>
+
+<style scoped lang="scss">
+.header{
+    font-family: Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
